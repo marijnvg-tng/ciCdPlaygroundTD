d03f29cf1d77869f051ac472d1d1a22f40ff627c
marijnvg-tng <marijn.vangeest@tngtech.com>
2025-01-23 18:05:04 +0100

    Personalize main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Marijn van Geest",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Continuous Integration/Development", "Seeing what you did immediately after committing a change", "somewhat real-time update of your changes", "tests to immediately check if you broke something"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "No tools :(",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["How to manage a CI/CD pipeline", "Decide which approach suits a certain type of project", "How to implement it on your own"],
   },
 });
